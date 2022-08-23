8629457f29dbf1b213b734e0030b07deec2db84e
Alec Delaney <tekktik@gmail.com>
2022-08-22 21:36:29 -0400

    Keep copyright up to date in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import datetime
 
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -39,7 +40,8 @@ master_doc = "index"
 
 # General information about the project.
 project = "Adafruit BME680 Library"
-copyright = "2017 ladyada"
+current_year = str(datetime.datetime.now().year)
+copyright = current_year + " ladyada"
 author = "ladyada"
 
 # The version info for the project you're documenting, acts as replacement for
